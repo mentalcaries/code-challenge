562cb5c908a26dfdc31f606206eefaab05057589
Devin Jaggernauth <neexis@gmail.com>
2024-07-12 22:58:41 -0400

    search within carousel

--- image_parser.rb
+++ image_parser.rb
@@ -11,6 +11,10 @@ document = Nokogiri::HTML.parse(open('./files/van-gogh-paintings.html'))
 
 tags = document.xpath("//a")
 
-tags.each do |tag|
+carousel_links = document.css('g-scrolling-carousel a')
+
+puts "There are #{carousel_links.count} images\n \n"
+
+carousel_links.each do |tag|
   puts tag[:title]
 end
