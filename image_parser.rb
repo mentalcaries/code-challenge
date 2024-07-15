d0d358cf2fed79c09c8a5ed9a721cb943a3523a5
Devin Jaggernauth <neexis@gmail.com>
2024-07-15 07:35:51 -0400

    get thumbnail ids

--- image_parser.rb
+++ image_parser.rb
@@ -1,20 +1,64 @@
 require 'nokogiri'
+require 'httparty'
 
 # "artworks" : [{
 #   "name": "",
 #   "extensions": ["year"],
 #   "link": "",
-#   "image"
+#   "image": ""
 # }]
 
 document = Nokogiri::HTML.parse(open('./files/van-gogh-paintings.html'))
 
-tags = document.xpath("//a")
+top_carousel = document.css('g-scrolling-carousel.wqBQjd')
+all_script_tags = document.css('script')
 
-carousel_links = document.css('g-scrolling-carousel a')
 
-puts "There are #{carousel_links.count} images\n \n"
+pattern = /var\s+s\s*=\s*'([^']+)';\s*var\s+ii/
 
-carousel_links.each do |tag|
-  puts tag[:title]
+data = { "artworks": [] }
+
+thumbnails = []
+
+def get_image_title(element, title_selector)
+  title_element = element.css(title_selector)
+  if title_element
+    title = title_element.text.strip.gsub(/\s+/, ' ')
+  return title
+  else
+    return ''
+  end
+end
+
+def get_image_src (element)
+  image = element.at_css('img')
+  if image
+    return image['src']
+  else
+    return ''
+  end
+end
+
+
+
+top_carousel.css('a').each do |link|
+
+  title = get_image_title(link, '.kltat')
+  # puts "Title: #{title}"
+
+  image = link.at_css('img')
+  image_id = image['id']
+
+  all_script_tags.each do |script|
+    match_data = script.content.match(/var\s+s\s*=\s*'([^']+)';\s*var\s+ii\s*=\s*\[\s*'#{image_id}'\s*\];/)
+    if match_data
+      thumbnails << match_data[1]
+    end
+  end
+
+  puts thumbnails
+
+
+
+
 end
