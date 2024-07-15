0365d98fde898aae7f74d9a7540da7131feb9907
Devin Jaggernauth <neexis@gmail.com>
2024-07-15 10:59:36 -0400

    clean up, output to JSON file

--- image_parser.rb
+++ image_parser.rb
@@ -15,29 +15,7 @@ document = Nokogiri::HTML.parse(open('./files/van-gogh-paintings.html'))
 top_carousel = document.css('g-scrolling-carousel.wqBQjd')
 all_script_tags = document.css('script')
 
-data = { "artworks": [] }
-
-
-# def get_element_text_content(element, title_selector)
-#   selected_element = element.css(title_selector)
-#   if selected_element
-#     text_content = selected_element.text.strip.gsub(/\s+/, ' ')
-#   return text_content
-#   else
-#     return ''
-#   end
-# end
-
-# def get_image_src (element)
-#   image = element.at_css('img')
-#   if image
-#     return image['src']
-#   else
-#     return ''
-#   end
-# end
-
-
+data = { "artworks" => [] }
 
 top_carousel.css('a').each do |link|
 
@@ -47,7 +25,6 @@ top_carousel.css('a').each do |link|
   image = link.at_css('img')
   image_id = image['id']
   thumbnail = nil
-  puts title
 
   all_script_tags.each do |script|
     match_data = script.content.match(/var\s+s\s*=\s*'([^']+)';\s*var\s+ii\s*=\s*\[\s*'#{image_id}'\s*\];/)
@@ -58,15 +35,16 @@ top_carousel.css('a').each do |link|
   end
 
   result = {
-    "name": title,
-    "extensions": extensions,
-    "link": image_url,
-    "image": thumbnail
+    "name"=> title,
+    "extensions"=> extensions,
+    "link"=> image_url,
+    "image"=> thumbnail
   }
 
-  data[:artworks] << result
+  data["artworks"] << result
 
 end
 
-
-puts JSON.pretty_generate(data)
+formatted_json = JSON.pretty_generate(data)
+puts formatted_json
+File.write('./image-parser-output.json', formatted_json)
